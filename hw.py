9b4c7a7767cc53d7fea18893c88502c2f7479976
Sunny2305 <137488522+Sunny2305@users.noreply.github.com>
2023-06-28 00:07:14 +0530

    Add files via upload

---
+++ hw.py
@@ -0,0 +1,6 @@
+n1 = int(input("Enter the number 1 :"))
+n2 = int(input("Enter the number 2 :"))
+n3 = int(input("Enter the number 3 :"))
+num = n1+n2+n3 
+avg = num/3
+print("The average of n1 ,n2 ,n3 is", avg)
